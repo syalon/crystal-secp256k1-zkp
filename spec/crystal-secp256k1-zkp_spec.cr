2032cb156499ba87f808ac728bd58440aace77fe
syalon <empty@email.com>
2021-09-22 10:35:30 +0800

    test06

--- spec/crystal-secp256k1-zkp_spec.cr
+++ spec/crystal-secp256k1-zkp_spec.cr
@@ -1,9 +1,14 @@
 require "./spec_helper"
 
-describe Crystal::Secp256k1::Zkp do
+describe Crystal::Secp256k1Zkp do
   # TODO: Write tests
 
   it "works" do
     false.should eq(true)
   end
+
+  it "init" do
+    Crystal::Secp256k1Zkp::LibSecp256k1.secp256k1_context_create(0).should eq(nil)
+  end
+
 end
--- src/crystal-secp256k1-zkp.cr
+++ src/crystal-secp256k1-zkp.cr
@@ -1,6 +1,13 @@
 # TODO: Write documentation for `Crystal::Secp256k1::Zkp`
-module Crystal::Secp256k1::Zkp
+module Crystal::Secp256k1Zkp
   VERSION = "0.1.0"
 
   # TODO: Put your code here
+
+  @[Link("secp256k1")]
+  lib LibSecp256k1
+    fun secp256k1_context_create(flag : Int32) : Void
+  end
+  
 end
+
