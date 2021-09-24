4c49a15b1768470187b3098eea2266af32e009b2
syalon <empty@email.com>
2021-09-24 15:20:39 +0800

    test10

--- spec/crystal-secp256k1-zkp_spec.cr
+++ spec/crystal-secp256k1-zkp_spec.cr
@@ -1,15 +1,9 @@
 require "./spec_helper"
 
-describe Crystal::Secp256k1Zkp do
+describe Secp256k1Zkp do
   # TODO: Write tests
 
-  # it "works" do
-  #   false.should eq(true)
-  # end
-
-  it "init" do
-    puts "here"
-    Crystal::Secp256k1Zkp::LibSecp256k1.secp256k1_context_create(0).should eq(true)
+  it "secp256k1_context_create" do
+    Secp256k1Zkp::LibSecp256k1.secp256k1_context_create(Secp256k1Zkp::LibSecp256k1::SECP256K1_CONTEXT_ALL).should_not be_nil
   end
-
 end
